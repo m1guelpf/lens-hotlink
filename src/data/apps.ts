0bda3e4151b2b31235878f44648b1fd6ecdb92ff
rtang03 <7686266+rtang03@users.noreply.github.com>
2022-06-01 10:06:45 +0800

    add madseed

--- src/data/apps.ts
+++ src/data/apps.ts
@@ -1,12 +1,13 @@
 import iris from './apps/iris'
 import sepana from './apps/sepana'
 import lenster from './apps/lenster'
+import madseed from './apps/madseed'
 import refract from './apps/refract'
 import { LensApp } from '@/types/lens'
 import lensfrens from './apps/lensfrens'
 
 const apps: Record<string, LensApp> = Object.fromEntries(
-	[iris, sepana, lenster, refract, lensfrens].map(app => [app.id, app])
+	[iris, sepana, lenster, refract, lensfrens, madseed].map(app => [app.id, app])
 )
 
 export default apps
